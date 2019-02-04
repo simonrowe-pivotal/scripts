2af31a2da629793336801c885d7d1baf52d87b07
Simon Rowe <srowe@pivotal.io>
2019-02-04 13:00:27 +0000

    move jmeter into newrelic folder

--- 3_deploy.sh
+++ 3_deploy.sh
@@ -15,6 +15,7 @@ deploy()
   if [ $? -eq 0 ]
   then
     echo "Successfully deployed $1"
+
   else
     echo "Could not deploy $1" >&2
     exit 1
@@ -29,9 +30,24 @@ autoscale()
     if [ ! "${app:0:1}" == "#" ]
     then
       app_name=`echo "$app" | cut -d " " -f 2`
-      cf delete-autoscaling-rules $app_name --force
       cf enable-autoscaling $app_name
       cf configure-autoscaling $app_name autoscaler-manifest.yml
+    fi
+  done < "$file"
+  wait
+}
+
+turnoffautoscalling()
+{
+  echo "Pausing for 15 seconds"
+  sleep 15
+  file="microServices.list"
+  while IFS= read -r app
+  do
+    if [ ! "${app:0:1}" == "#" ]
+    then
+      app_name=`echo "$app" | cut -d " " -f 2`
+      cf delete-autoscaling-rules $app_name --force
     fi
   done < "$file"
   wait
@@ -55,6 +71,7 @@ main()
   summaryOfServices
 }
 
+turnoffautoscalling
 main
 autoscale
 
--- jmeter/README.md
+++
@@ -1,6 +0,0 @@
-
-```
-cp credentials.yml.sample credentials.yml
-fly -t ci sp -p jmeter -c pipeline.yml -l credentials.yml
-fly -t ci tj -j jmeter/perf-test --watch
-```
